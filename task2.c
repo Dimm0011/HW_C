0346ac285104198e24cfd446046c03dc22ef07d0
Dimm0011 <dimmpchelin@gmail.com>
2024-10-28 18:30:34 +0400

    first commit

---
+++ task2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(void)
+{
+    setlocale( LC_ALL, "en_US.UTF-8");
+    printf("   *\n");
+    printf("    ***\n");
+    printf("   *****\n");
+    printf("  *******\n");
+    printf("   HHOHH\n");
+    printf("   ZZZZZ\n");
+    return 0;
+}
+
+   *
+    ***
+   *****
+  *******
+   HHOHH
+   ZZZZZ
